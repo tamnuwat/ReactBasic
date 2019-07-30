3fae04adf11b5cd8e837b368b1902ae5b795e8b1
tamnuwat <tamnuwatv@gmail.com>
2019-07-30 22:47:19 +0700

    State
    State

--- Basic/App.js
+++ Basic/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default class App extends React.Component {
-   render() {
-      return (
-         <View >
-            <Text>Welcome to React</Text>
-            <Button title = "OK" Color ="#841584"></Button>
-         </View>
-      );
-   }
+class Blink extends Component {
+
+  componentDidMount(){
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  //state object
+  state = { isShowingText: true };
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  }
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='Welcome to React' />
+        <Button title = 'ok' Color ="#841584"/>
+      </View>
+    );
+  }
 }
