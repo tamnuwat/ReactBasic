050e41edeee1fc84235a4cf407c906200ae97cf2
tamnuwat <tamnuwatv@gmail.com>
2019-07-30 21:42:46 +0700

    Prop
    Prop

--- Basic/App.js
+++ Basic/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default class App extends React.Component {
    render() {
       return (
-         <View>
-            <Text>Welcome to ReactNative</Text>
+         <View >
+            <Text>Welcome to React</Text>
+            <Button title = "OK" Color ="#841584"></Button>
          </View>
       );
    }
